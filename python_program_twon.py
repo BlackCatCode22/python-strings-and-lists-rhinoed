9f460954eee388a8dc991303f7df07a6dbde1e7f
Mark Edmunds <edmundsmd1@gmail.com>
2023-08-25 23:47:17 -0700

    seting up document adding todos

---
+++ python_program_twon.py
@@ -0,0 +1,45 @@
+"""
+Class: CIT 95
+Name: Mark Edmunds
+Assignment: GitHub classroom program two
+"""
+text = "Python is an amazing programming language. It is versatile, easy to learn, and powerful."
+# Strings
+# TODO: write a function that take a string and returns the number of character in the string
+
+# TODO: write code that returns text to uppercase followed by lowercase
+
+# TODO: write code that returns that word count of text
+
+# TODO: write code that replaces a user chosen word with another user chosen word in the text string
+
+# TODO: remove all leading and trailing white space in text
+
+# TODO: write a function that takes a string and returns a list with the sentences contained in the string.
+
+# TODO: write code that reverse each word of text
+
+# TODO: write python code that take a character and returns the number of occurrences of it in text
+
+# TODO: write python code that take a substring and returns the number of occurrences of it in text
+# Lists
+# TODO: create a list called word_list which has each word in text
+
+# TODO: append the word 'Pythonic' to word_list
+
+# TODO: insert the word 'awesome' to the  beginning of word_list
+
+# TODO: print the third word in the word_list and then print a sublist containing the words from the 6th to 9th position.
+
+# TODO: remove the word "amazing" from the word_list
+
+# TODO: Sort the word_list in alphabetical order.
+
+# TODO: count the occurrences of the word "is" in the word_list
+
+# TODO: create a string sentence by joining the words in the word_list with spaces
+
+# TODO: reverse the order of elements in the word_list
+
+# TODO: create a new list copied_list by copying the contents of the word_list
+
